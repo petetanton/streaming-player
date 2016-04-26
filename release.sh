650954292fe2db3e117dda101bea0ead65aa5250
petetanton <pete.tanton@bbc.co.uk>
2016-04-26 22:13:43 +0100

    add jenkins kick to release script

--- release.sh
+++ release.sh
@@ -4,3 +4,9 @@ rm release.properties
 mvn clean
 mvn install
 mvn release:prepare
+
+
+scm_tag=$(grep "^scm.tag=" "release.properties" | cut -d'=' -f2)
+
+curl -X POST http://192.168.1.5:8080/job/sr-player-build/build \
+  --data-urlencode json='{"parameter": [{"name":"tag", "value":"'"${scm_tag}"'"}]}'
