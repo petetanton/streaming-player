2b56f7d5cd40e0b2d8bf0183e94a56beeae53d8b
petetanton <pete.tanton@bbc.co.uk>
2016-04-26 22:13:28 +0100

    put service wrapper in right place

--- src/main/scripts/post_install.sh
+++ src/main/scripts/post_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo cp /etc/sr-lbp/streaming-player-service-wrapper.sh /etc/init.d/streaming-player
+sudo cp /etc/sr-lbp/streaming-player/streaming-player-service-wrapper.sh /etc/init.d/streaming-player
 sudo chmod +x /etc/init.d/streaming-player
 sudo service httpd stop
 sudo service streaming-player stop
