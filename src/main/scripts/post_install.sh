60e3aeead3dc9778754a9167114a533dcb02125a
petetanton <pete.tanton@bbc.co.uk>
2016-04-26 22:31:04 +0100

    moved httpd conf to be copied after install

--- src/main/scripts/post_install.sh
+++ src/main/scripts/post_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo cp /etc/sr-lbp/streaming-player/streaming-player-service-wrapper.sh /etc/init.d/streaming-player
+sudo cp /etc/sr-lbp/streaming-player/httpd.conf /etc/httpd/conf/httpd.conf
 sudo chmod +x /etc/init.d/streaming-player
 sudo service httpd stop
 sudo service streaming-player stop
