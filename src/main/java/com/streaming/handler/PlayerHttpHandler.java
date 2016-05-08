24b1fb906fb398086efb59bb0280cf403cdce383
petetanton <pete.tanton@bbc.co.uk>
2016-05-08 10:51:27 +0100

    make player responsive

--- src/main/java/com/streaming/handler/PlayerHttpHandler.java
+++ src/main/java/com/streaming/handler/PlayerHttpHandler.java
@@ -20,11 +20,18 @@ public class PlayerHttpHandler extends HttpHandler {
         response.setHeader("Content-Type", "text/html");
         response.setHeader("Access-Control-Allow-Origin", "*");
         StringBuilder sb = new StringBuilder();
-        sb.append("<html><head>");
-        sb.append("<script type=\"text/javascript\" src=\"https://s3-eu-west-1.amazonaws.com/sr-static-hosting/player/src/clappr/0-2-49/clappr.min.js\"></script>");
-        sb.append("<script type=\"text/javascript\" src=\"https://s3-eu-west-1.amazonaws.com/sr-static-hosting/player/src/clappr-level-selector/0-1-10/level-selector.min.js\"></script>");
-        sb.append("</head>");
-        sb.append("<body>");
+        sb.append("<!DOCTYPE html>\n" +
+                "<!--[if lt IE 7]>      <html lang=\"en\" class=\"no-js lt-ie9 lt-ie8 lt-ie7\"> <![endif]-->\n" +
+                "<!--[if IE 7]>         <html lang=\"en\" class=\"no-js lt-ie9 lt-ie8 ie7\"> <![endif]-->\n" +
+                "<!--[if IE 8]>         <html lang=\"en\" class=\"no-js lt-ie9 ie8\"> <![endif]-->\n" +
+                "<!--[if IE 9]>         <html lang=\"en\" class=\"no-js lt-ie10 ie9\"> <![endif]-->\n" +
+                "<!--[if !IE]><!--> <html lang=\"en\" class=\"no-js\">             <!--<![endif]-->\n");
+        sb.append("<head>\n");
+        sb.append("<meta charset=\"utf-8\">\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
+        sb.append("<script type=\"text/javascript\" src=\"https://s3-eu-west-1.amazonaws.com/sr-static-hosting/player/src/clappr/0-2-49/clappr.min.js\"></script>\n");
+        sb.append("<script type=\"text/javascript\" src=\"https://s3-eu-west-1.amazonaws.com/sr-static-hosting/player/src/clappr-level-selector/0-1-10/level-selector.min.js\"></script>\n");
+        sb.append("</head>\n");
+        sb.append("<body>\n");
 
         final String streamId = request.getParameter("id");
         final Stream stream;
@@ -55,8 +62,8 @@ public class PlayerHttpHandler extends HttpHandler {
             return;
         }
 
-        sb.append("<div id=\"player\"></div>");
-        sb.append("<script>");
+        sb.append("<div id=\"player\" style=\"width:640px; height=360px;\"></div>\n");
+        sb.append("<script>\n");
         sb.append("var player = new Clappr.Player({source: \"");
         sb.append(streamManifestUrl);
         sb.append("\", maxBufferLength:240, useDvrControls: true, parentId: \"#player\", mediacontrol: {seekbar: \"#66B2FF\", buttons: \"#00000\"}, gaAccount: 'UA-73120346-1',\n" +
@@ -89,7 +96,17 @@ public class PlayerHttpHandler extends HttpHandler {
                 "    ga('create', 'UA-73120346-1', 'auto');\n" +
                 "    ga('send', 'pageview');\n" +
                 "\n" +
-                "</script>");
+                "</script>\n");
+
+        sb.append("<script>\nfunction resizePlayer(){\n" +
+                "    var aspectRatio = 9/16,\n" +
+                "    newWidth = document.getElementById('player').parentElement.offsetWidth,\n" +
+                "    newHeight = 2 * Math.round(newWidth * aspectRatio/2);\n" +
+                "    player.resize({width: newWidth, height: newHeight});\n" +
+                "  }\n" +
+                "\n" +
+                "  resizePlayer();\n" +
+                "  window.onresize = resizePlayer; </script>");
 
         sb.append("</body>");
         sb.append("</html>");
